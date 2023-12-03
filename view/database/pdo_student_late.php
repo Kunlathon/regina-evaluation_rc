cc1b7158f40c88e7224381090e2c8613b4319a49
beer_code_2023 <mpamese.pc2001@gmail.com>
2023-12-03 09:52:09 +0700

    update 2023/12/03 09:52

--- view/database/pdo_student_late.php
+++ view/database/pdo_student_late.php
@@ -9,25 +9,27 @@
                     $student_late_server="localhost";
 					$student_late_username="root";
 					$student_late_password="053282395";
-					$student_late_db="student_late";
+					$student_late_db="rc_student_late";
 					$student_late_port=3399;
                     try{
                         $connto_student_late=new PDO("mysql:host=$student_late_server;dbname=$student_late_db;port=$student_late_port;charset=utf8",$student_late_username,$student_late_password);
                         $connto_student_late->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
                     }catch(PDOException $e){
-                        echo "Connection failed: ->pdo_student_late<-";
+                        $connto_student_late=null;
+                        echo "Connection failed: ->rc_student_late<-";
                     }
                 }else{
                     $student_late_server="localhost";
 					$student_late_username="Regina@ict2022";
 					$student_late_password="Regina@ict2022";
-					$student_late_db="student_late";
+					$student_late_db="rc_student_late";
 					$student_late_port=3306;
                     try{
                         $connto_student_late=new PDO("mysql:host=$student_late_server;dbname=$student_late_db;port=$student_late_port;charset=utf8",$student_late_username,$student_late_password);
                         $connto_student_late->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
                     }catch(PDOException $e){
-                        echo "Connection failed: ->pdo_student_late<-";
+                        $connto_student_late=null;
+                        echo "Connection failed: ->rc_student_late<-";
                     }
                 }
             $this->connto_student_late=$connto_student_late;	
